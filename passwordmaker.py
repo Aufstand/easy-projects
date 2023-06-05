d367eb795f88486252bc4427332503c2aa397f28
Aufstand <gomezromanr@gmail.com>
2023-06-05 14:37:43 +0200

    Update passwordmaker.py
    Bettered

--- passwordmaker.py
+++ passwordmaker.py
@@ -3,10 +3,18 @@ letters = 'abcdefghijklmnopqrstuvwxyz'
 numbers = '0123456789'
 characters = r"""!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"""
 letters_mayus = letters.upper() # Quería probar si funcionaba
-base = 0
+counter = 0
+while True:
+    try:
+        passlen = int(input("Insert lenght of password:"))
+        if passlen <= 0:
+            raise ValueError
+        break
+    except ValueError:
+        print("Do you have stupid?")
 password = ""
-while base < 15:
-    base += 1
+while counter < passlen:
+    counter += 1
     first_choice = random.choice([letters, numbers, letters_mayus, characters])
     char_choice = random.choice(first_choice)
     password += char_choice
